020d36874b9ed40b6fde6be087575cd8f22225f3
SantiagoHerreG <888@holbertonschool.com>
2019-08-16 20:46:13 +0000

    holberton.h header file updated

--- holberton.h
+++ holberton.h
@@ -1,9 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#define _POSIX_C_SOURCE 200809L
 char *_strcpy(char *, char *);
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *_strcat(char *dest, char *src);
+void my_exit(char* exit_status, char ** command);
 
 #endif /* HOLBERTON_H */
