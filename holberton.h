3ac8afaec0e0992993966776a077333700d06f54
SantiagoHerreG <888@holbertonschool.com>
2019-08-18 03:41:46 +0000

    header file updated

--- holberton.h
+++ holberton.h
@@ -7,7 +7,7 @@ char *_strcpy(char *, char *);
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *_strcat(char *dest, char *src);
-void my_exit(char* exit_status, char ** command);
+void my_exit(char *exit_status, char **command);
 int _atoi(char *);
 short _getline(char **command, size_t *size, int fd);
 void (*signal(int, void (*)(int)))(int);
