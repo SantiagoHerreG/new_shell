8ee0ee21cfe503fb8840118a5abd6ec499102c3f
SantiagoHerreG <888@holbertonschool.com>
2019-08-24 18:43:52 +0000

    added some code for alias expansion, not finished

--- alias_expansion.c
+++ alias_expansion.c
@@ -1,10 +1,30 @@
 #include "holberton.h"
 
-int alias_expansion(char *av[], char *alias[], char *command)
+
+int alias_expansion(char *av[], char *alias[], char *arguments)
 {
-	int i = 1;
-	av[1] = av[1];
-	alias[1] = alias[1];
-	command = command;
-	return (i);
+       int i = 0;
+       char al_name[ARG_MAX], *token;
+       while (alias[i])
+       {
+               _strcpy(al_name, alias[i]);
+               _strtok(al_name, "=");
+               eq_len = _strlen(al_name);
+               if (!_strcmp(al_name, av[0]) && alias[i][eq_len] == '=')
+                       break;
+               i++;
+       }
+       if (!alias[i])
+               return (0);
+       token = _strtok(NULL, "=");
+       j = 1, i = 0;
+       if (token[j] != '\'')
+               av[0][i++] = token[j++];
+       while (token[j + 1])
+       {
+               if (token[j] != '\'')
+                       av[0][i++] = token[j++];
+       }
+       av[0][i] = '\0';
+       return (1);
 }
